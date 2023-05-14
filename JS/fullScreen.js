4b12d37f2d8bd96fb77b5846c63613ea8597ed58
Jeremy Cloarec <jeremycloarec@msn.com>
2023-05-14 20:34:52 +0200

    suite

---
+++ JS/fullScreen.js
@@ -0,0 +1,44 @@
+console.log("hello");
+
+// let cv = document.querySelector('#cv');
+// console.log(cv);
+
+
+// function openFullscreen() {
+//     if (cv.requestFullscreen) {
+//     cv.requestFullscreen();
+//   } else if (cv.webkitRequestFullscreen) { /* Safari */
+//     cv.webkitRequestFullscreen();
+//   } else if (cv.msRequestFullscreen) { /* IE11 */
+//     cv.msRequestFullscreen();
+//   }
+// }
+
+// cv.addEventListener('click', event => {
+//     openFullscreen();
+// });
+
+
+
+
+
+const cv = document.querySelector('.cv')
+
+const fullscreen = () => {
+    if (document.fullscreenElement){
+        document.exitFullscreen();
+    }
+    
+    else{
+        cv.requestFullscreen()
+    }
+}
+
+cv.addEventListener('click', fullscreen)
+
+
+
+
+
+
+
